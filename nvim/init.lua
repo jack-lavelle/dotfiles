d23ddaf83f4cecd6de450628e41616cb10bf1dab
Jack LaVelle <jacklavelle17@gmail.com>
2025-11-11 16:22:41 -0500

    disable copilot for markdown files

--- nvim/init.lua
+++ nvim/init.lua
@@ -973,12 +973,12 @@ require('lazy').setup({
     config = function()
       vim.g.copilot_no_tab_map = true
       vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-      -- vim.api.nvim_create_autocmd('FileType', {
-      --   pattern = { 'markdown' },
-      --   callback = function()
-      --     vim.b.copilot_enabled = false
-      --   end,
-      -- })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'markdown' },
+        callback = function()
+          vim.b.copilot_enabled = false
+        end,
+      })
     end,
   },
 
